f17de3ea8308d89d121d8ad973284cf9946d654c
AselaSamarakoon <asela@kloudynet.com>
2023-11-07 14:16:05 +0800

    Add AutoMapper package and configure mapper services

---
+++ KN.KloudIdentity.Mapper/Utils/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using KN.KloudIdentity.Mapper.Config;
+
+namespace KN.KloudIdentity.Mapper;
+
+public class AutoMapperConfig
+{
+    public static AutoMapper.Mapper InitializeAutoMapper()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<MapperConfig, AppConfigModel>();
+            cfg.CreateMap<AuthConfig, AuthConfigModel>();
+            cfg.CreateMap<SchemaAttribute, UserSchemaModel>();
+            cfg.CreateMap<SchemaAttribute, GroupSchemaModel>();
+
+            cfg.CreateMap<AppConfigModel, MapperConfig>();
+        });
+
+        return new AutoMapper.Mapper(config);
+    }
+}
---
+++ KN.KloudIdentity.Mapper/Utils/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using KN.KloudIdentity.Mapper.Config;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KN.KloudIdentity.Mapper;
+
+public static class ServiceExtension
+{
+    public static void ConfigureMapperServices(this IServiceCollection services)
+    {
+        services.AddScoped<AutoMapperConfig>();
+        services.AddScoped<IAuthContext, AuthContextV1>();
+        services.AddScoped<IAuthStrategy, ApiKeyStrategy>();
+        services.AddScoped<IAuthStrategy, BasicAuthStrategy>();
+        services.AddScoped<IAuthStrategy, OAuth2Strategy>();
+        services.AddScoped<IConfigReader, ConfigReaderSQL>();
+    }
+}
