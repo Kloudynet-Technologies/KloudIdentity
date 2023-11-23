a093fc0daf05f41e7d63ff540ef9e24fadf61ec4
arifulkloudynet <ariful@kloudynet.com>
2023-11-23 11:26:42 +0600

    - create update group class

---
+++ KN.KloudIdentity.Mapper/MapperCore/Group/UpdateGroup.cs
@@ -0,0 +1,25 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Kloudynet Technologies Sdn Bhd.  All rights reserved.
+//------------------------------------------------------------
+
+using Microsoft.SCIM;
+
+namespace KN.KloudIdentity.Mapper.MapperCore.Group
+{
+    public class UpdateGroup : OperationsBase<Core2Group>, IUpdateResource<Core2Group>
+    {
+        public UpdateGroup(IConfigReader configReader, IAuthContext authContext) : base(configReader, authContext)
+        {
+        }
+
+        public override Task MapAndPreparePayloadAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Resource> UpdateAsync(Resource resource, string appId, string correlationID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
