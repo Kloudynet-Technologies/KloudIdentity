debfbe717f6541be5a88b66595ddadfa78af099d
arifulkloudynet <ariful@kloudynet.com>
2023-12-01 09:36:50 +0600

    - add appId path

--- Microsoft.SystemForCrossDomainIdentityManagement/Service/ServiceConstants.cs
+++ Microsoft.SystemForCrossDomainIdentityManagement/Service/ServiceConstants.cs
@@ -7,7 +7,7 @@ namespace Microsoft.SCIM
         public const string PathSegmentResourceTypes = "ResourceTypes";
         public const string PathSegmentSchemas = "Schemas";
         public const string PathSegmentServiceProviderConfiguration = "ServiceProviderConfig";
-        public const string RouteGroups = SchemaConstants.PathInterface + ServiceConstants.SeparatorSegments + ProtocolConstants.PathGroups;
+        public const string RouteGroups = SchemaConstants.PathInterface + ServiceConstants.SeparatorSegments + SchemaConstants.ApplicationIdentifier + ServiceConstants.SeparatorSegments + ProtocolConstants.PathGroups;
         public const string RouteResourceTypes = SchemaConstants.PathInterface + ServiceConstants.SeparatorSegments + ServiceConstants.PathSegmentResourceTypes;
         public const string RouteSchemas = SchemaConstants.PathInterface + ServiceConstants.SeparatorSegments + ServiceConstants.PathSegmentSchemas;
         public const string RouteServiceConfiguration = SchemaConstants.PathInterface + ServiceConstants.SeparatorSegments + ServiceConstants.PathSegmentServiceProviderConfiguration;
