f7020feefb4491e14a807b4b23494532862dabbe
AselaSamarakoon <asela@kloudynet.com>
2024-02-14 14:30:06 +0800

    Update Zoho role and profile IDs

--- KN.KloudIdentity.MapperOverride/User/CreateUser-Zoho.cs
+++ KN.KloudIdentity.MapperOverride/User/CreateUser-Zoho.cs
@@ -24,8 +24,8 @@ public class CreateUser_Zoho : CreateUser
                     ["first_name"] = Resource.Name.GivenName,
                     ["last_name"] = Resource.Name.FamilyName,
                     ["email"] = Resource.UserName,
-                    ["role"] = "6073302000000026005",
-                    ["profile"] = "6073302000000026014"
+                    ["role"] = "6138980000000026005",
+                    ["profile"] = "6138980000000026014"
                 }
             }
         };
