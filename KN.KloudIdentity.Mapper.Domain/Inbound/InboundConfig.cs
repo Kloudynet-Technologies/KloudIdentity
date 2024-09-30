4cd2d87f133364b1ee77e515f78f4cf561202828
AselaSamarakoon <asela@kloudynet.com>
2024-09-30 15:39:55 +0800

    feat(Inbound provisioning - Entra ID) - Inbound attribute mapping algorithm
    - Inbound mapping algorithm
    - DateTime added to JsonDataTypes
    
    Tasks: #9

---
+++ KN.KloudIdentity.Mapper.Domain/Inbound/InboundConfig.cs
@@ -0,0 +1,24 @@
+﻿using KN.KloudIdentity.Mapper.Domain.Application;
+using KN.KloudIdentity.Mapper.Domain.Authentication;
+using KN.KloudIdentity.Mapper.Domain.Mapping.Inbound;
+
+namespace KN.KloudIdentity.Mapper.Domain.Inbound;
+
+public record InboundConfig
+{
+    public string AppId { get; init; } = string.Empty;
+
+    public string AADAppClientId { get; init; } = string.Empty;
+
+    public string AADAppObjectId { get; init; } = string.Empty;
+
+    public IntegrationMethods IntegrationMethodInbound { get; init; }
+
+    public AuthenticationMethods AuthenticationMethodInbound { get; init; }
+
+    public required dynamic AuthenticationDetails { get; set; }
+
+    public string InboundAttMappingUsersPath { get; init; } = string.Empty;
+
+    public IList<InboundAttributeMapping> InboundAttributeMappings { get; init; } = [];
+}
