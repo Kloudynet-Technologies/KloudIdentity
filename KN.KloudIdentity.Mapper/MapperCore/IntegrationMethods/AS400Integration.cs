e88e67d0a4945c119ca41ff8d00258b49ad95fd9
Ariful Islam <ariful@kloudynet.com>
2025-01-13 14:58:56 +0600

    feat(As400): enhance group validation logic and improve error handling
    Tasks: #28

--- KN.KloudIdentity.Mapper/MapperCore/IntegrationMethods/AS400Integration.cs
+++ KN.KloudIdentity.Mapper/MapperCore/IntegrationMethods/AS400Integration.cs
@@ -18,106 +18,62 @@ namespace KN.KloudIdentity.Mapper.MapperCore;
 /// <summary>
 /// Represents an AS400 integration.
 /// </summary>
-public class AS400Integration : IIntegrationBase
+public class AS400Integration(
+    IReqStagQueuePublisher reqStagQueuePublisher,
+    IOptions<AppSettings> appSettings,
+    IListAs400GroupsQuery listAs400GroupsQuery)
+    : IIntegrationBase
 {
-    public IntegrationMethods IntegrationMethod { get; init; }
-    private readonly JSONParserUtilV2<Core2EnterpriseUser> _jsonParserUtilV2;
-    private readonly IReqStagQueuePublisher _reqStagQueuePublisher;
-    private readonly IOptions<AppSettings> _appSettings;
-    private readonly IListAs400GroupsQuery _listAs400GroupsQuery;
+    public IntegrationMethods IntegrationMethod { get; init; } = IntegrationMethods.AS400;
 
     private readonly string _urnPrefix = "urn:kn:ki:schema:";
 
-    public AS400Integration(
-        IReqStagQueuePublisher reqStagQueuePublisher, 
-        IOptions<AppSettings> appSettings,
-        IListAs400GroupsQuery listAs400GroupsQuery
-        )
-    {
-        IntegrationMethod = IntegrationMethods.AS400;
-        _jsonParserUtilV2 = new JSONParserUtilV2<Core2EnterpriseUser>();
-        _reqStagQueuePublisher = reqStagQueuePublisher;
-        _listAs400GroupsQuery = listAs400GroupsQuery;
-        _appSettings = appSettings;
-    }
-
     public async Task DeleteAsync(string identifier, AppConfig appConfig, string correlationId)
     {
-        var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, default);
-
-        var apiPath = appConfig.IntegrationDetails!.TrimEnd('/') + "/api/USERS?identifier=" + identifier;
-
-        var as400RequestMessage = new AS400RequestMessage(
-            apiPath,
-            basicAuth.Username,
-            basicAuth.Password,
-            string.Empty
-        ); 
-
-        var responseMessage = await SendMessage(correlationId, as400RequestMessage, OperationTypes.Delete, default);
+        var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, CancellationToken.None);
+        var apiPath = CombineApiPath(appConfig.IntegrationDetails, $"/api/USERS?identifier={identifier}");
+        
+        var requestMessage = new AS400RequestMessage(apiPath, basicAuth.Username, basicAuth.Password, string.Empty);
+        var responseMessage = await SendMessage(correlationId, requestMessage, OperationTypes.Delete, CancellationToken.None);
 
         if (responseMessage == null || responseMessage?.IsError == true)
-        {
             throw new HttpRequestException($"Error occurred while deleting the user: {responseMessage?.ErrorMessage}");
-        }
     }
 
     public async Task<Core2EnterpriseUser> GetAsync(string identifier, AppConfig appConfig, string correlationId, CancellationToken cancellationToken = default)
     {
         var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, cancellationToken);
+        var apiPath = CombineApiPath(appConfig.IntegrationDetails, $"/api/USERS?identifier={identifier}");
 
-        var apiPath = appConfig.IntegrationDetails!.TrimEnd('/')+ "/api/USERS?identifier=" + identifier;
+        var requestMessage = new AS400RequestMessage(apiPath, basicAuth.Username, basicAuth.Password, string.Empty);
+        var response = await SendMessage(correlationId, requestMessage, OperationTypes.List, cancellationToken);
 
-        var as400RequestMessage = new AS400RequestMessage(
-            apiPath,
-            basicAuth.Username,
-            basicAuth.Password,
-            string.Empty
-        );
-
-        var response = await SendMessage(correlationId, as400RequestMessage, OperationTypes.List, cancellationToken);
-
-        var user = new Core2EnterpriseUser();
-
-        if (response == null || response?.IsError == true)
-        {
+        if (response == null || response.IsError == true)
             throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
-        }
 
-        List<AS400User> userList = JsonConvert.DeserializeObject<List<AS400User>>(response!.Message.ToString());
+        List<AS400User> users = JsonConvert.DeserializeObject<List<AS400User>>(response.Message.ToString());
+        var userFields = users?.FirstOrDefault(u => u.Identifier == identifier);
 
-        var userFields = userList.Count > 0 ? userList.FirstOrDefault(p => p.Identifier == identifier) : null;
-              
-        if (userFields != null)
+        if (userFields == null)
+            throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+        return new Core2EnterpriseUser
         {
-            user.UserName = userFields.Username;
-            user.Identifier = userFields.Identifier;
-
-            return user;
-        }
-
-        throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+            UserName = userFields.Username,
+            Identifier = userFields.Identifier
+        };
     }
 
     public async Task<dynamic> GetAuthenticationAsync(AppConfig config, SCIMDirections direction = SCIMDirections.Outbound, CancellationToken cancellationToken = default)
     {
-        if (config?.AuthenticationDetails == null)
-        {
+        if (config.AuthenticationDetails == null)
             throw new HttpRequestException("Authentication details are missing.");
-        }
 
         var basicAuth = JsonConvert.DeserializeObject<BasicAuthentication>(config.AuthenticationDetails.ToString())
                         ?? throw new ArgumentException("Invalid authentication details.");
 
-        if (string.IsNullOrWhiteSpace(basicAuth.Username))
-        {
-            throw new HttpRequestException("Username is missing.");
-        }
-
-        if (string.IsNullOrWhiteSpace(basicAuth.Password))
-        {
-            throw new HttpRequestException("Password is missing.");
-        }
+        if (string.IsNullOrWhiteSpace(basicAuth.Username) || string.IsNullOrWhiteSpace(basicAuth.Password))
+            throw new HttpRequestException("Username or Password is missing.");
 
         return await Task.FromResult(basicAuth);
     }
@@ -125,10 +81,8 @@ public class AS400Integration : IIntegrationBase
     public async Task<dynamic> MapAndPreparePayloadAsync(IList<AttributeSchema> schema, Core2EnterpriseUser resource, CancellationToken cancellationToken = default)
     {
         if (!ValidateAttributeSchema(schema))
-        {
             throw new HttpRequestException("Invalid attribute schema.");
-        }
-
+        
         string groupProfile = string.Empty;
         string supplementalGroupProfile = string.Empty;
         var userClassValue = schema.FirstOrDefault(x => x.DestinationField.Contains("UserClass"))!.SourceValue;
@@ -177,11 +131,8 @@ public class AS400Integration : IIntegrationBase
     public async Task ProvisionAsync(dynamic payload, AppConfig appConfig, string correlationId, CancellationToken cancellationToken = default)
     {
         await ValidatePayloadAsync(payload,appConfig, correlationId, cancellationToken);
-
         var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, default);
-
-        var apiPath = appConfig.IntegrationDetails!.TrimEnd('/') + "/api/USERS";
-
+        var apiPath = CombineApiPath(appConfig.IntegrationDetails, "/api/USERS");
         var requestPayload = new
         {
             correlationId = correlationId,
@@ -189,47 +140,28 @@ public class AS400Integration : IIntegrationBase
         };
 
         string jsonStringPayload = JsonConvert.SerializeObject(requestPayload);
-
-        var as400RequestMessage = new AS400RequestMessage(
-            apiPath,
-            basicAuth.Username,
-            basicAuth.Password,
-            jsonStringPayload
-        );
-
+        var as400RequestMessage = new AS400RequestMessage(apiPath, basicAuth.Username, basicAuth.Password, jsonStringPayload);
         var responseMessage = await SendMessage(correlationId, as400RequestMessage, OperationTypes.Create, cancellationToken);
-
+        
         if (responseMessage == null || responseMessage?.IsError == true)
-        {
             throw new HttpRequestException($"Error occurred while creating the user: {responseMessage?.ErrorMessage}");
-        }
     }
 
     private async Task<StagingQueueResponseMessage> SendMessage(string correlationId, AS400RequestMessage as400RequestMessage, OperationTypes operationType, CancellationToken cancellationToken)
     {
-        StagingQueueRequestMessage request = new(
-            correlationId,
-            as400RequestMessage,
-            HostTypes.AS400,
-            operationType
-        );
-
+        StagingQueueRequestMessage request = new(correlationId, as400RequestMessage, HostTypes.AS400, operationType);
         var encryptedMessage = EncryptWithPrivateKey(request);
-
-        var responseMessage = await _reqStagQueuePublisher.SendAsync(encryptedMessage, correlationId, operationType, cancellationToken);
-
+        var responseMessage = await reqStagQueuePublisher.SendAsync(encryptedMessage, correlationId, operationType, cancellationToken);
         var result = JsonConvert.DeserializeObject<StagingQueueResponseMessage>(responseMessage);
 
         return result!;
     }
+    
     private string EncryptWithPrivateKey(StagingQueueRequestMessage message)
     {
-        // Retrieve the private key from Azure Key Vault
-        string privateKey = _appSettings.Value.ExternalQueueEncryptionKey;
-
+        string privateKey = appSettings.Value.ExternalQueueEncryptionKey;
         using RSA rsa = RSA.Create();
         rsa.ImportFromPem(privateKey.ToCharArray());
-
         string messageJson = JsonConvert.SerializeObject(message);
         byte[] encryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(messageJson), RSAEncryptionPadding.Pkcs1);
 
@@ -239,8 +171,8 @@ public class AS400Integration : IIntegrationBase
     public async Task ReplaceAsync(dynamic payload, Core2EnterpriseUser resource, AppConfig appConfig, string correlationId)
     {
         ValidatePayloadAsync(payload, appConfig, correlationId, CancellationToken.None);
-        var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, default);
-        var apiPath = appConfig.IntegrationDetails!.TrimEnd('/') + "/api/USERS";
+        var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, CancellationToken.None);
+        var apiPath = CombineApiPath(appConfig.IntegrationDetails, "/api/USERS");
 
         var requestPayload = new
         {
@@ -255,27 +187,18 @@ public class AS400Integration : IIntegrationBase
         };
 
         string jsonStringPayload = JsonConvert.SerializeObject(requestPayload);
-
-        var as400RequestMessage = new AS400RequestMessage(
-            apiPath,
-            basicAuth.Username,
-            basicAuth.Password,
-            jsonStringPayload
-        );
-
-        var responseMessage = await SendMessage(correlationId, as400RequestMessage, OperationTypes.Update, default);
-
+        var as400RequestMessage = new AS400RequestMessage(apiPath, basicAuth.Username, basicAuth.Password, jsonStringPayload);
+        var responseMessage = await SendMessage(correlationId, as400RequestMessage, OperationTypes.Update, CancellationToken.None);
+        
         if (responseMessage == null || responseMessage?.IsError == true)
-        {
             throw new HttpRequestException($"Error occurred while updating the user: {responseMessage?.ErrorMessage}");
-        }
     }
 
     public async Task UpdateAsync(dynamic payload, Core2EnterpriseUser resource, AppConfig appConfig, string correlationId)
     {
         await ValidatePayloadAsync(payload, appConfig, correlationId, CancellationToken.None);
-        var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, default);
-        var apiPath = appConfig.IntegrationDetails!.TrimEnd('/') + "/api/USERS";
+        var basicAuth = await GetAuthenticationAsync(appConfig, SCIMDirections.Outbound, CancellationToken.None);
+        var apiPath = CombineApiPath(appConfig.IntegrationDetails, "/api/USERS");
 
         var requestPayload = new
         {
@@ -291,104 +214,60 @@ public class AS400Integration : IIntegrationBase
 
         string jsonStringPayload = JsonConvert.SerializeObject(requestPayload);
 
-        var as400RequestMessage = new AS400RequestMessage(
-            apiPath,
-            basicAuth.Username,
-            basicAuth.Password,
-            jsonStringPayload
-        );
-
-        var responseMessage = await SendMessage(correlationId, as400RequestMessage, OperationTypes.Update, default);
+        var as400RequestMessage = new AS400RequestMessage(apiPath, basicAuth.Username, basicAuth.Password, jsonStringPayload);
+        var responseMessage = await SendMessage(correlationId, as400RequestMessage, OperationTypes.Update, CancellationToken.None);
 
         if (responseMessage == null || responseMessage?.IsError == true)
-        {
             throw new HttpRequestException($"Error occurred while updating the user: {responseMessage?.ErrorMessage}");
-        }
     }
 
     public async Task<(bool, string[])> ValidatePayloadAsync(dynamic payload, AppConfig appConfig, string correlationId, CancellationToken cancellationToken = default)
     { 
-        var groups = new List<As400Group>();
-        
-        var username = payload["username"];
-        var userClass = payload["userClass"];
-        var groupProfile = payload["groupProfile"];
-        var supplementalGroupProfile = payload["supplementalGroupProfile"];
+        var groups = string.IsNullOrEmpty(payload["groupProfile"])
+            ? []
+            : (List<As400Group>)await listAs400GroupsQuery.ListAsync(appConfig.AppId, cancellationToken);
 
-        if (!string.IsNullOrEmpty(groupProfile))
-        {
-            groups = (List<As400Group>)await _listAs400GroupsQuery.ListAsync(appConfig.AppId, cancellationToken);        
-        }
-        
-        ValidateUsername(username);
-        ValidateUserClass(userClass);
-        ValidateGroupProfile(groupProfile, groups);
-        ValidateSupplementalGroupProfile(supplementalGroupProfile, groups);
-        
-        if(string.IsNullOrEmpty(groupProfile) && !string.IsNullOrEmpty(supplementalGroupProfile))
-        {
+        ValidateUsername(payload["username"]);
+        ValidateUserClass(payload["userClass"]);
+        ValidateGroupProfile(payload["groupProfile"], groups);
+        ValidateSupplementalGroupProfile(payload["supplementalGroupProfile"], groups);
+
+        if (string.IsNullOrEmpty(payload["groupProfile"]) && !string.IsNullOrEmpty(payload["supplementalGroupProfile"]))
             throw new HttpRequestException("GroupProfile must be provided when SupplementalGroupProfile is provided.");
-        }
-        
-        return await Task.FromResult((true, Array.Empty<string>()));
+
+        return (true, []);
     }
 
     private static bool ValidateAttributeSchema(IList<AttributeSchema> schema)
     {
         if (schema == null || schema.Count == 0)
-        {
             throw new HttpRequestException("Attribute schema is empty.");
-        }
 
         if (schema.Any(x => string.IsNullOrEmpty(x.SourceValue) || string.IsNullOrEmpty(x.DestinationField)))
-        {
             throw new HttpRequestException("Source value or destination field is empty.");
-        }
 
         return true;
     }
 
     private static void ValidateUsername(string username)
     {
-        if (username.Length > 10)
-        {
-            throw new HttpRequestException("Username must be at most 10 characters long.");
-        }
-
-        if (!username.All(char.IsLetterOrDigit))
-        {
-            throw new HttpRequestException("Username must not contain special characters.");
-        }
+        if (string.IsNullOrWhiteSpace(username) || username.Length > 10 || !username.All(char.IsLetterOrDigit))
+            throw new HttpRequestException("Invalid Username.");
     }
 
     private static void ValidateUserClass(string userClass)
     {
         var validUserClasses = new[] { "*USER", "*PGMR" };
         if (!validUserClasses.Contains(userClass))
-        {
             throw new HttpRequestException("UserClass must be either '*USER' or '*PGMR'.");
-        }
     }
     
     private static void ValidateGroupProfile(string groupProfile, List<As400Group> groups)
     {
-        if(string.IsNullOrEmpty(groupProfile)) return;
-        
         var isExistingGroup = groups.Any(x => x.GroupName == groupProfile);
-        if (!isExistingGroup)
-        {
-            throw new HttpRequestException($"{groupProfile} is not a valid group.");
-        }
         
-        if (groupProfile.Length > 10)
-        {
-            throw new HttpRequestException($"{groupProfile} must be at most 10 characters long.");
-        }
-
-        if (!groupProfile.All(char.IsLetterOrDigit))
-        {
-            throw new HttpRequestException($"{groupProfile} must not contain special characters.");
-        }
+        if (!string.IsNullOrEmpty(groupProfile) && (!isExistingGroup || groupProfile.Length > 10 || !groupProfile.All(char.IsLetterOrDigit)))
+            throw new HttpRequestException("Invalid GroupProfile.");
     } 
     
     private static void ValidateSupplementalGroupProfile(string supplementalGroupProfile, List<As400Group>groups)
@@ -397,18 +276,18 @@ public class AS400Integration : IIntegrationBase
         
         var totalGroups = supplementalGroupProfile.Split(' ');
         if (supplementalGroupProfile.Length > 200)
-        {
             throw new HttpRequestException("SupplementalGroupProfile must be at most 200 characters long.");
-        }
         
         if (totalGroups.Length > 16)
-        {
             throw new HttpRequestException("SupplementalGroupProfile must not contain more than 16 groups.");
-        }
-
-        foreach (var group in totalGroups)
-        {
-           ValidateGroupProfile(group, groups);
-        }
+        
+        var isNotExistingGroup = totalGroups.Any(group => groups.All(x => x.GroupName != group));
+        if (isNotExistingGroup)
+            throw new HttpRequestException("Invalid SupplementalGroupProfile.");
+    }
+    
+    private string CombineApiPath(string basePath, string endpoint)
+    {
+        return basePath.TrimEnd('/') + endpoint;
     }
 }
