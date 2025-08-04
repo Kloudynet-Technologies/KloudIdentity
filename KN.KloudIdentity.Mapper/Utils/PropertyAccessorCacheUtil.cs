33f319efb7b88122cdcb5b9ed5074ed74ed35768
Ariful Islam <ariful@kloudynet.com>
2025-08-04 11:45:00 +0600

    refactor(PropertyAccessorCacheUtil): Replace tuple cache key with a dedicated struct for improved clarity and type safety
    Tasks: #38

--- KN.KloudIdentity.Mapper/Utils/PropertyAccessorCacheUtil.cs
+++ KN.KloudIdentity.Mapper/Utils/PropertyAccessorCacheUtil.cs
@@ -1,18 +1,43 @@
 using System;
 using System.Reflection;
 using System.Collections.Concurrent;
+using System.Linq.Expressions;
 
 namespace KN.KloudIdentity.Mapper.Utils;
 
 public static class PropertyAccessorCacheUtil
 {
-    private static readonly ConcurrentDictionary<(Type, string), Func<object, object>> _cache = new();
+    private readonly struct CacheKey : IEquatable<CacheKey>
+    {
+        public Type Type { get; }
+        public string PropertyName { get; }
+
+        public CacheKey(Type type, string propertyName)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            PropertyName = propertyName ?? throw new ArgumentNullException(nameof(propertyName));
+        }
+
+        public bool Equals(CacheKey other) =>
+            Type == other.Type &&
+            StringComparer.OrdinalIgnoreCase.Equals(PropertyName, other.PropertyName);
+
+        public override bool Equals(object? obj) =>
+            obj is CacheKey other && Equals(other);
+
+        public override int GetHashCode() =>
+            HashCode.Combine(Type, StringComparer.OrdinalIgnoreCase.GetHashCode(PropertyName));
+    }
+
+    private static readonly ConcurrentDictionary<CacheKey, Func<object, object?>> _cache = new();
 
     public static string? GetPropertyValue(object obj, string propertyName)
     {
         if (obj == null) throw new ArgumentNullException(nameof(obj));
+        if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+
         var type = obj.GetType();
-        var key = (type, propertyName.ToLowerInvariant());
+        var key = new CacheKey(type, propertyName);
 
         if (!_cache.TryGetValue(key, out var accessor))
         {
@@ -20,11 +45,11 @@ public static class PropertyAccessorCacheUtil
             if (prop == null)
                 throw new ArgumentException($"Property '{propertyName}' not found on {type.Name}.", nameof(propertyName));
 
-            var param = System.Linq.Expressions.Expression.Parameter(typeof(object), "obj");
-            var cast = System.Linq.Expressions.Expression.Convert(param, type);
-            var propertyAccess = System.Linq.Expressions.Expression.Property(cast, prop);
-            var convertResult = System.Linq.Expressions.Expression.Convert(propertyAccess, typeof(object));
-            accessor = System.Linq.Expressions.Expression.Lambda<Func<object, object>>(convertResult, param).Compile();
+            var param = Expression.Parameter(typeof(object), "obj");
+            var cast = Expression.Convert(param, type);
+            var propertyAccess = Expression.Property(cast, prop);
+            var convertResult = Expression.Convert(propertyAccess, typeof(object));
+            accessor = Expression.Lambda<Func<object, object?>>(convertResult, param).Compile();
 
             _cache[key] = accessor;
         }
