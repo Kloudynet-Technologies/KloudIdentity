5769b79cfa77dec6918b934ae25a625e45552fc8
Ariful Islam <ariful@kloudynet.com>
2025-08-20 14:32:03 +0600

    chore(TokenController): Add copyright notice to the top of the file

--- Microsoft.SCIM.WebHostSample/Controllers/TokenController.cs
+++ Microsoft.SCIM.WebHostSample/Controllers/TokenController.cs
@@ -1,3 +1,6 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.IdentityModel.Tokens.Jwt;
