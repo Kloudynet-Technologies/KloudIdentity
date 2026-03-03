359c448a25acd83f0e5e57bd352d6d45258c017f
Ariful Islam <ariful@kloudynet.com>
2026-03-03 10:24:09 +0600

    feat: Add OnModelCreating method to configure entity mappings in KNContext

--- KN.KloudIdentity.Mapper.Infrastructure/Persistence/SQLServer/KNContext.cs
+++ KN.KloudIdentity.Mapper.Infrastructure/Persistence/SQLServer/KNContext.cs
@@ -6,4 +6,10 @@ namespace KN.KloudIdentity.Mapper.Infrastructure.Persistence.SQLServer;
 public class KNContext(DbContextOptions<KNContext> options) : DbContext(options)
 {
    public DbSet<AppConfigSnapshot> AppConfigSnapshots { get; set; } = null!;
+   
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof(KNContext).Assembly);
+   }
 }
